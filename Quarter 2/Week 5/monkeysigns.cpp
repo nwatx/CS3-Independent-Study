424039ba562d93f2cdbaf6eb97cc37043f55e962
Neo Wang <43394822+nwatx@users.noreply.github.com>
2020-11-21 16:16:31 -0600

    11/21/2020

---
+++ Quarter 2/Week 5/monkeysigns.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ar array
+#define vt vector
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define F0R(i, a) for(int i = 0; i < (a); i++)
+#define trav(a, x) for (auto& a : x)
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pi;
+typedef pair<long, long> pl;
+typedef pair<double, double> pd;
+
+typedef vt<int> vi;
+typedef vt<ld> vd;
+typedef vt<ll> vl;
+typedef vt<pi> vpi;
+typedef vt<pl> vpl;
+
+#define pb push_back
+#define mp make_pair
+#define sz(x) (int)x.size()
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) beg(x), en(x)
+
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const ld PI = 4*atan((ld)1);
+
+namespace input {
+    template<class T> void re(complex<T>& x);
+    template<class T1, class T2> void re(pair<T1,T2>& p);
+    template<class T> void re(vector<T>& a);
+    template<class T, size_t SZ> void re(array<T,SZ>& a);
+
+    template<class T> void re(T& x) { cin >> x; }
+    void re(double& x) { string t; re(t); x = stod(t); }
+    void re(ld& x) { string t; re(t); x = stold(t); }
+    template<class Arg, class... Args> void re(Arg& first, Args&... rest) { 
+        re(first); re(rest...); 
+    }
+
+    template<class T> void re(complex<T>& x) { T a,b; re(a,b); x = cd(a,b); }
+    template<class T1, class T2> void re(pair<T1,T2>& p) { re(p.f,p.s); }
+    template<class T> void re(vector<T>& a) { F0R(i,sz(a)) re(a[i]); }
+    template<class T, size_t SZ> void re(array<T,SZ>& a) { F0R(i,SZ) re(a[i]); }
+}
+
+using namespace input;
+
+void setIn(string s) { freopen(s.c_str(),"r",stdin); }
+void setOut(string s) { freopen(s.c_str(),"w",stdout); }
+void setIO(string s = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    if (sz(s)) { setIn(s+".in"), setOut(s+".out"); }
+}
+
+
+int main() {
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    string line; getline(cin, line);
+
+    int mC = 0;
+
+    vi freq(26);
+
+    F0R(i, sz(line)) {
+        line[i] = tolower(line[i]);
+        if(line[i] == ' ') continue;
+        if(line[i] == 'm') mC++;
+        else freq[line[i] - 'a']++;
+    }
+
+    int ret = 0;
+    trav(a, freq) {
+        ret = max(ret, a);
+    }
+
+    cout << mC + ret;
+}
---
+++ Quarter 2/Week 5/moop.cpp
@@ -0,0 +1,160 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define ar array
+#define vt vector
+#define f first
+#define s second
+#define lb lower_bound
+#define ub upper_bound
+
+#define FOR(i, a, b) for(int i = (a); i < (b); i++)
+#define F0R(i, a) for(int i = 0; i < (a); i++)
+#define trav(a, x) for (auto& a : x)
+
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pi;
+typedef pair<long, long> pl;
+typedef pair<double, double> pd;
+
+typedef vt<int> vi;
+typedef vt<ld> vd;
+typedef vt<ll> vl;
+typedef vt<pi> vpi;
+typedef vt<pl> vpl;
+
+#define pb push_back
+#define mp make_pair
+#define sz(x) (int)x.size()
+#define beg(x) x.begin()
+#define en(x) x.end()
+#define all(x) beg(x), en(x)
+
+const int MAX_N = 1e5 + 1;
+const int MOD = 1e9 + 7;
+const int INF = 1e9;
+const ll LINF = 1e18;
+const ld PI = 4*atan((ld)1);
+
+namespace input {
+    template<class T> void re(complex<T>& x);
+    template<class T1, class T2> void re(pair<T1,T2>& p);
+    template<class T> void re(vector<T>& a);
+    template<class T, size_t SZ> void re(array<T,SZ>& a);
+
+    template<class T> void re(T& x) { cin >> x; }
+    void re(double& x) { string t; re(t); x = stod(t); }
+    void re(ld& x) { string t; re(t); x = stold(t); }
+    template<class Arg, class... Args> void re(Arg& first, Args&... rest) { 
+        re(first); re(rest...); 
+    }
+
+    template<class T> void re(complex<T>& x) { T a,b; re(a,b); x = cd(a,b); }
+    template<class T1, class T2> void re(pair<T1,T2>& p) { re(p.f,p.s); }
+    template<class T> void re(vector<T>& a) { F0R(i,sz(a)) re(a[i]); }
+    template<class T, size_t SZ> void re(array<T,SZ>& a) { F0R(i,SZ) re(a[i]); }
+}
+
+using namespace input;
+
+void setIn(string s) { freopen(s.c_str(),"r",stdin); }
+void setOut(string s) { freopen(s.c_str(),"w",stdout); }
+void setIO(string s = "") {
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    if (sz(s)) { setIn(s+".in"), setOut(s+".out"); }
+}
+
+namespace output {
+    template<class T1, class T2> void pr(const pair<T1,T2>& x);
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x);
+    template<class T> void pr(const vector<T>& x);
+    template<class T> void pr(const set<T>& x);
+    template<class T1, class T2> void pr(const map<T1,T2>& x);
+
+    template<class T> void pr(const T& x) { cout << x; }
+    template<class Arg, class... Args> void pr(const Arg& first, const Args&... rest) { 
+        pr(first); pr(rest...); 
+    }
+
+    template<class T1, class T2> void pr(const pair<T1,T2>& x) { 
+        pr("{",x.f,", ",x.s,"}"); 
+    }
+    template<class T> void prContain(const T& x) {
+        pr("{");
+        bool fst = 1; for (const auto& a: x) pr(!fst?", ":"",a), fst = 0; // const needed for vector<bool>
+        pr("}");
+    }
+    template<class T, size_t SZ> void pr(const array<T,SZ>& x) { prContain(x); }
+    template<class T> void pr(const vector<T>& x) { prContain(x); }
+    template<class T> void pr(const set<T>& x) { prContain(x); }
+    template<class T1, class T2> void pr(const map<T1,T2>& x) { prContain(x); }
+    
+    void ps() { pr("\n"); }
+    template<class Arg> void ps(const Arg& first) { 
+        pr(first); ps(); // no space at end of line
+    }
+    template<class Arg, class... Args> void ps(const Arg& first, const Args&... rest) { 
+        pr(first," "); ps(rest...); // print w/ spaces
+    }
+}
+
+using namespace output;
+
+int N;
+
+struct UF {
+    vi e;
+    UF(int n) : e(n, -1) {}
+    bool sameSet(int a, int b) { return find(a) == find(b); }
+    int size(int x) { return -e[find(x)]; }
+    int find(int x) { return e[x] < 0 ? x : e[x] = find(e[x]); }
+    bool join(int a, int b) {
+        a = find(a), b = find(b);
+        if (a == b) return false;
+        if (e[a] > e[b]) swap(a, b);
+        e[a] += e[b]; e[b] = a;
+        return true;
+    }
+};
+
+bool del(pi a, pi b) {
+    return (a.f >= b.f && a.s >= b.s) || (a.f <= b.f && a.s <= b.s);
+}
+
+bool visited[MAX_N];
+pi pt[MAX_N];
+
+int main() {
+    clock_t tStart = clock();
+
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    setIO("moop"); cin >> N;
+
+    UF uf(N);
+    F0R(i, N) { re(pt[i]); }
+
+    sort(pt, pt+N);
+
+    int ret = N;
+
+    F0R(i, N) {
+        if(visited[i]) continue;
+        visited[i] = true;
+        FOR(j, i+1, N) {
+            if(del(pt[i], pt[j]) && uf.join(i, j)) {
+            //if((pt[i].f >= pt[j].f && pt[i].s >= pt[j].s) || (pt[i].f <= pt[j].f && pt[i].s <= pt[j].s) && uf.join(i, j)) {
+                visited[j] = true;
+                --ret;
+            }
+        }
+    }
+
+    cout << ret;
+    cerr << "\n" << (double)(clock() - tStart) / CLOCKS_PER_SEC;
+
+    //pr(pt);
+}
